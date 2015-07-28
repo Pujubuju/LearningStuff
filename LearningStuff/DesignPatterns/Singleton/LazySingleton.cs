039318262d8e84e11f7ba8c46bafec311642eeb7
Pujubuju <truetormentedsoul@gmail.com>
2015-07-28 19:30:12 +0200

    singleton

---
+++ LearningStuff/DesignPatterns/Singleton/LazySingleton.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton
+{
+    public class LazySingleton
+    {
+
+        private readonly object _locker = new object();
+
+        private LazySingleton _instance;
+
+        public LazySingleton Instance
+        {
+            get
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LazySingleton();                       
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        private LazySingleton()
+        {         
+        }
+    }
+}
---
+++ LearningStuff/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+
+        private readonly Singleton _instance = new Singleton();
+
+        public Singleton Instance { get { return _instance; } }
+
+        private Singleton()
+        {         
+        }
+    }
+}
