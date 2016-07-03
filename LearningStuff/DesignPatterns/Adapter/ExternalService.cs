3afbedbe9d8b6bf3ac2ef67d9ea3f74fb18b14eb
pujubuju <truetormentedsoul@gmail.com>
2016-07-03 09:09:47 +0200

    dd

--- LearningStuff/DesignPatterns/Adapter/ExternalService.cs
+++ LearningStuff/DesignPatterns/Adapter/ExternalService.cs
@@ -1,10 +1,92 @@
-﻿namespace Adapter
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Adapter
 {
-    public class ExternalService
+    public interface IExternalService
     {
-        public int GetNumberOfUsers()
+        IEnumerable<ExternalUser> GetAll();
+    }
+
+    public class ExternalUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string About { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+
+
+    public class UsersService1
+    {
+        private readonly IExternalService _externalService;
+
+        public UsersService1(IExternalService externalService)
         {
-            return 234;
+            _externalService = externalService;
+        }
+
+        public IEnumerable<ExternalUser> GetUsersByName(string name)
+        {
+            return
+                _externalService
+                .GetAll()
+                .Where(x => x.FirstName == name);
+        }
+    }
+
+
+    public class ExternalServiceAdapter : IExternalServiceAdapter
+    {
+        private readonly IExternalService _externalService;
+
+        public ExternalServiceAdapter(IExternalService externalService)
+        {
+            _externalService = externalService;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _externalService
+                .GetAll()
+                .Select(x => new User
+            {
+                Name = x.FirstName,
+                Surname = x.LastName,
+                Age = x.Age
+            });
+        } 
+    }
+
+    public interface IExternalServiceAdapter
+    {
+        IEnumerable<User> GetAll();
+    }
+
+    public class UsersService
+    {
+        private readonly IExternalServiceAdapter _externalServiceAdapter;
+
+        public UsersService(IExternalServiceAdapter externalServiceAdapter)
+        {
+            _externalServiceAdapter = externalServiceAdapter;
+        }
+
+        public IEnumerable<User> GetUsersByName(string name)
+        {
+            return
+                _externalServiceAdapter
+                .GetAll()
+                .Where(x => x.Name == name);
         }
     }
 }
